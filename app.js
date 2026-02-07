a61bb9ef3fb76fb310931006df5b8b428b000d0c
Amy Trice <amylizt@gmail.com>
2026-02-07 17:07:42 -0500

    added wendy's page

--- app.js
+++ app.js
@@ -2,17 +2,31 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Import the data file you created
+const familyMembers = require('./members'); 
+
 app.set('view engine', 'ejs');
 
-// THIS LINE IS CRITICAL: It makes the 'public' folder accessible to the browser
+// Serves images/css from the public folder
 app.use('/familytree', express.static(path.join(__dirname, 'public')));
+
+// The Main Tree Page
 app.get('/familytree', (req, res) => {
     res.render('index');
 });
 
+app.get('/familytree/profile/:id', (req, res) => {
+    const memberId = req.params.id;
+    const memberData = familyMembers[memberId];
+
+    if (memberData) {
+        res.render('profile', { member: memberData });
+    } else {
+        res.status(404).send("Family member not found. Check if the ID matches in members.js");
+    }
+});
 
 const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(`HVAC site live at http://localhost:${PORT}/familytree`);
+    console.log(`Site live at http://localhost:${PORT}/familytree`);
 });
-
---
+++ members.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "wendy": {
+        name: "Wendy Wilson",
+        birthday: "March 14, 1976",
+        location: "Philadelphia, PA",
+        knownFor: "Amazing photography, Peanut Gallery Humor, and Perfectly Salted Food!",
+        mainImg: "wendy.png",
+        album: [
+            { src: "wendyblue.jpg", caption: "The Blue Hair ERA!" },
+            { src: "wendykitty.jpg", caption: "She loves kitties!!" },
+            { src: "wendyamydressy.jpg", caption: "Dressing Up :) " },
+            { src: "dadwendygranny.jpg", caption: "With Granny at Christmas" }
+        ]
+    },
+    "dad": {
+        name: "Dad",
+        birthday: "January 15, 1970",
+        location: "Seattle, WA",
+        knownFor: "Fixing anything with duct tape",
+        mainImg: "Dad.png",
+        album: [
+            { src: "tools.jpg", caption: "In the workshop" }
+        ]
+    }
+    // You can easily add 100 more people here without cluttering your logic
+};
