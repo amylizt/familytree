43ce96d0ee877d8ab1f487d5509a6d2c750ed163
Amy Trice <amylizt@gmail.com>
2026-02-07 07:41:47 -0500

    more pipeline fixes

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ app.set('view engine', 'ejs');
 // THIS LINE IS CRITICAL: It makes the 'public' folder accessible to the browser
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/familyphotos', (req, res) => {
+app.get('/familytree', (req, res) => {
     res.render('index');
 });
 
 app.listen(PORT, () => {
-    console.log(`Family Photos site live at http://localhost:${PORT}/familyphotos`);
+    console.log(`Family Photos site live at http://localhost:${PORT}/familytree`);
 });
